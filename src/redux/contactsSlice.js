d23c1a71a3a63d1154d494a1fa0c0afe004712e0
Andriy Babiy <andriybabiy97@gmail.com>
2024-05-12 21:17:58 +0200

    Fix to form of contacts initial state

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,12 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { v4 } from 'uuid';
 
-const INITIAL_STATE = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+const INITIAL_STATE = {
+  contacts: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+};
 
 const contactsSlice = createSlice({
   name: 'contacts',
